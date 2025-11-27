821992e230b9852576a4efcaaac2d69193f349e3
MarcPickel <marcpc@att.net>
2025-11-26 23:56:38 -0600

    Add: dropdown menu

---
+++ src/utils/constants.js
@@ -0,0 +1,185 @@
+const psalter = [
+  {
+    number: 1,
+    translation: "RSVCE",
+    title: "",
+    subtitle: "",
+    pretext: "",
+    text: "",
+  },
+  {
+    number: 2,
+    title: "",
+    subtitle: "",
+    pretext: "",
+    text: "",
+  },
+  {
+    number: 3,
+    title: "",
+    subtitle: "",
+    pretext: "",
+    text: "",
+  },
+  {
+    number: 4,
+    title: "",
+    subtitle: "",
+    pretext: "",
+    text: "",
+  },
+  {
+    number: 5,
+    title: "",
+    subtitle: "",
+    pretext: "",
+    text: "",
+  },
+  {
+    number: 6,
+    title: "",
+    subtitle: "",
+    pretext: "",
+    text: "",
+  },
+  {
+    number: 7,
+    title: "",
+    subtitle: "",
+    pretext: "",
+    text: "",
+  },
+  {
+    number: 8,
+    title: "",
+    subtitle: "",
+    pretext: "",
+    text: "",
+  },
+  {
+    number: 9,
+    title: "",
+    subtitle: "",
+    pretext: "",
+    text: "",
+  },
+  {
+    number: 10,
+    title: "",
+    subtitle: "",
+    pretext: "",
+    text: "",
+  },
+  {
+    number: 11,
+    title: "",
+    subtitle: "",
+    pretext: "",
+    text: "",
+  },
+  {
+    number: 12,
+    title: "",
+    subtitle: "",
+    pretext: "",
+    text: "",
+  },
+  {
+    number: 13,
+    title: "",
+    subtitle: "",
+    pretext: "",
+    text: "",
+  },
+  {
+    number: 14,
+    title: "",
+    subtitle: "",
+    pretext: "",
+    text: "",
+  },
+  {
+    number: 15,
+    title: "",
+    subtitle: "",
+    pretext: "",
+    text: "",
+  },
+  {
+    number: 16,
+    title: "",
+    subtitle: "",
+    pretext: "",
+    text: "",
+  },
+  {
+    number: 17,
+    title: "",
+    subtitle: "",
+    pretext: "",
+    text: "",
+  },
+  {
+    number: 18,
+    title: "",
+    subtitle: "",
+    pretext: "",
+    text: "",
+  },
+  {
+    number: 19,
+    title: "",
+    subtitle: "",
+    pretext: "",
+    text: "",
+  },
+  {
+    number: 20,
+    title: "",
+    subtitle: "",
+    pretext: "",
+    text: "",
+  },
+  {
+    number: 21,
+    title: "",
+    subtitle: "",
+    pretext: "",
+    text: "",
+  },
+  {
+    number: 22,
+    title: "",
+    subtitle: "",
+    pretext: "",
+    text: "",
+  },
+  {
+    number: 23,
+    title: "Psalm 23",
+    subtitle: "The Divine Shepherd",
+    pretext: "A Psalm of David",
+    text: `
+            1 The Lord is my shepherd, I shall not want;<br />
+            2 he makes me lie down in green pastures. He leads me beside still
+            waters;<br />
+            3 he restores my soul. He leads me in paths of righteousness for his
+            name’s sake.<br />
+            4 Even though I walk through the valley of the shadow of death, I
+            fear no evil; for thou art with me; thy rod and thy staff, they
+            comfort me.<br />
+            5 Thou preparest a table before me in the presence of my enemies;
+            thou anointest my head with oil, my cup overflows.<br />
+            6 Surely goodness and mercy shall follow me all the days of my life;
+            and I shall dwell in the house of the Lord for ever.`,
+  },
+  {
+    number: 24,
+    title: "",
+    subtitle: "",
+    pretext: "",
+    text: "",
+  },
+];
+
+export { psalter };
